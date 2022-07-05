0aa7b4bc4a62804d8d7b257bdfa8116d7b2374b9
haziq-khurshid <haziq.khurshid@gmail.com>
2022-07-05 16:50:14 +0500

    Create a spotify playlist using terraform

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.2.6"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+provider "spotify" {
+  api_key = var.spotify_api_key
+}
+
+data "spotify_search_track" "by_artist" {
+  artist = "Post Malone"
+  album = "Twelve Carat Toothache"
+  # name  = "Cooped Up"
+}
+
+resource "spotify_playlist" "playlist" {
+  name        = "Terraform Playlist"
+  description = "This playlist was created by Terraform"
+  public      = true
+
+  tracks = [
+    data.spotify_search_track.by_artist.tracks[0].id,
+    data.spotify_search_track.by_artist.tracks[1].id,
+    data.spotify_search_track.by_artist.tracks[3].id,
+    data.spotify_search_track.by_artist.tracks[4].id,
+    data.spotify_search_track.by_artist.tracks[5].id,
+  ]
+}
